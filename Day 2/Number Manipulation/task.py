abb81cae1ca70d7c9f944c4180fcb30acda767f2
Gr1mm4g3 <grimmage@protonmail.com>
2024-12-16 14:40:49 +0100

    F-strings to insert variables into strings

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,6 +1,5 @@
 score = 0
+height = 1.8
+is_winning = True
 
-# User scores a point
-score += 1
-
-print(score)
+print(f"Your score is = {score}, your height = {height}. You are winning is {is_winning}")
