ccd9679dc99b1026c14312080c6580a6e59e0d8a
Gr1mm4g3 <grimmage@protonmail.com>
2024-12-18 19:34:36 +0100

    Modulo

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,7 @@
+# Even number 12 % 2 == 0
+number = int(input("Enter a number:\n"))
+
+if (number % 2) == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
